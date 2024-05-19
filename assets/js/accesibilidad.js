8903875f5167ab0f1f99c7a3202b8b81bab4b0e5
a <a>
2024-05-19 21:08:33 +0200

    Added basic elements

---
+++ assets/js/accesibilidad.js
@@ -0,0 +1,35 @@
+const applyColorTheme = (() => {
+	const COLOR_TOGGLER = document.getElementById('color-toggle');
+	const PREFERRED_THEME = localStorage.getItem('Theme');
+	PREFERRED_THEME === 'light' ? COLOR_TOGGLER.toggleAttribute('open') : localStorage.setItem('Theme', 'dark');
+	COLOR_TOGGLER.onclick = () => updateColorTheme();
+})();
+
+const updateColorTheme = () => {
+	const CURRENT_THEME = localStorage.getItem('Theme');
+	let newTheme = CURRENT_THEME === "dark" ? "light" : "dark";
+	localStorage.setItem('Theme', newTheme);
+}
+
+
+function mouseoverHandler() {
+	this.toggleAttribute('open', true);
+}
+
+function mouseoutHandler() {
+	this.toggleAttribute('open', false);
+}
+
+const addEventListeners = () => {
+	document.querySelector('nav details').addEventListener('mouseover', mouseoverHandler);
+	document.querySelector('nav details').addEventListener('mouseout', mouseoutHandler);
+}
+
+const removeEventListeners = () => {
+	document.querySelector('nav details').removeEventListener('mouseover', mouseoverHandler);
+	document.querySelector('nav details').removeEventListener('mouseout', mouseoutHandler);
+}
+
+window.innerWidth >= 1083 ? addEventListeners() : null;
+
+window.addEventListener('resize', () => window.innerWidth >= 1083 ? addEventListeners() : removeEventListeners());
